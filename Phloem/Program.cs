1e96a304f29f58878c1792b23ffdbf16b347eb98
MokkaCicc <baptiste.velten@hotmail.fr>
2021-03-16 09:30:37 +0100

    Project strucure

---
+++ Phloem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Phloem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Phloem
+
+High performance Conway's Game of Life written in C#.
+Icon made by Freepik perfect from [www.flaticon.com](www.flaticon.com).
